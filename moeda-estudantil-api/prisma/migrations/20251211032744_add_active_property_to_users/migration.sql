8ac40114343a16fbc795bd3c2714a0f98315b34b
Andre <andrejales.com@gmail.com>
2025-12-11 00:28:13 -0300

    feat: drop approved column from Company model and add active column to User model

---
+++ moeda-estudantil-api/prisma/migrations/20251211032744_add_active_property_to_users/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `approved` on the `Company` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Company" DROP COLUMN "approved";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT false;
